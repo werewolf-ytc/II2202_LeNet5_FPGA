356d19689ea88a69abad59031f3be90cda22181d
Tiancheng Yang <58246222+werewolf-ytc@users.noreply.github.com>
2021-10-04 21:33:42 +0200

    Pooling Layer Synthesized
    Passed Synthesis and  Implementation.

--- FullyConnetedLayer.vhd
+++ FullyConnetedLayer.vhd
@@ -6,12 +6,15 @@ use work.ParameterPkg.all;
 
 
 Entity FullyConnectedLayer is
+  Generic (
+    matrix_width : integer := 4
+    );
   Port(
     clk_in : IN std_logic;
     reset_in : IN std_logic;
     
-    data_in : IN Narray (My_number_of_inputs - 1 downto 0);
-    weight_in : IN Narray (My_number_of_inputs - 1 downto 0);
+    data_in : IN Narray (matrix_width * matrix_width - 1 downto 0);
+    weight_in : IN Narray (matrix_width * matrix_width - 1 downto 0);
     bias_in : IN data_type;
     
     result_out : OUT data_type
@@ -19,9 +22,9 @@ Entity FullyConnectedLayer is
 end FullyConnectedLayer;
 
 Architecture FullyParallel of FullyConnectedLayer is
-  signal Input_data_sig, Input_weight_sig : Narray (My_number_of_inputs downto 0);
+  signal Input_data_sig, Input_weight_sig : Narray (matrix_width * matrix_width downto 0);
   signal REG_sig : data_type;
-  signal Out_arr_sig		  : Narray (0 to My_number_of_inputs + 1);
+  signal Out_arr_sig		  : Narray (0 to matrix_width * matrix_width + 1);
     component MACBasicN is
 		Generic(
 			Int_length :integer;
@@ -34,12 +37,14 @@ Architecture FullyParallel of FullyConnectedLayer is
 	end component;
       
   begin 
-	Input_weight_sig (My_number_of_inputs downto 1) <= weight_in (My_number_of_inputs - 1 downto 0);
+	Input_weight_sig (matrix_width * matrix_width downto 1)
+	    <= weight_in (matrix_width * matrix_width - 1 downto 0);
 	Input_weight_sig (0) <= (My_frac_length => '1', others => '0');
 	Input_data_sig (0) <= bias_in;
-	Input_data_sig (My_number_of_inputs downto 1) <= data_in (My_number_of_inputs - 1 downto 0);
+	Input_data_sig (matrix_width * matrix_width downto 1)
+	    <= data_in (matrix_width * matrix_width - 1 downto 0);
     Out_arr_sig (0) <= (others => '0');
-    U0 : FOR i IN 0 TO My_number_of_inputs GENERATE
+    U0 : FOR i IN 0 TO matrix_width * matrix_width GENERATE
 		MAC_a : MACBasicN
 			generic map(
 				Int_length => My_int_length,
@@ -58,7 +63,7 @@ Architecture FullyParallel of FullyConnectedLayer is
 		if(reset_in = '1')then
 			REG_sig <= (others => '0');
 		elsif (rising_edge (clk_in)) then
-			REG_sig <= Out_arr_sig(My_number_of_inputs + 1);
+			REG_sig <= Out_arr_sig(matrix_width * matrix_width + 1);
 		end if;
 	end process;
 	result_out <= REG_sig;
---
+++ PoolingLayer.vhd
@@ -0,0 +1,70 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.ParameterPkg.all;
+
+entity PoolingLayer is
+    Generic (
+        input_matrix_width : integer;
+        filter_width : Integer;
+        stride : Integer
+        );
+    Port (
+        clk_in : IN std_logic;
+        reset_in : IN std_logic;
+        
+        matrix_in : IN Narray (input_matrix_width * input_matrix_width - 1
+            downto 0);
+            
+        matrix_out : OUT Narray (
+            (input_matrix_width - filter_width + stride ) / stride  -- output width
+            ** 2 -1 downto 0)
+        );
+end PoolingLayer;
+
+architecture MaxPoolBehavioral of PoolingLayer is
+CONSTANT output_matrix_width : INTEGER
+    := (input_matrix_width - filter_width + stride ) / stride;
+    
+-- Max pool the input matrix
+FUNCTION MaxPoolingFunction (
+    matrix_in : IN Narray (input_matrix_width * input_matrix_width - 1
+            downto 0)
+    ) RETURN Narray IS
+    VARIABLE matrix_return : Narray (
+            (input_matrix_width - filter_width + stride ) / stride  -- output width
+            ** 2 -1 downto 0);
+    VARIABLE local_max_var : data_type;        
+BEGIN
+    for i in output_matrix_width - 1 downto 0 loop
+        for j in output_matrix_width - 1 downto 0 loop
+        -- for each pooled value
+        
+            local_max_var := matrix_in (Matrix2VectorIndexConvertion(
+                input_matrix_width, i, j));
+            for p in filter_width - 1 downto 0 loop
+                for q in filter_width - 1 downto 0 loop
+                    -- find the max within a window
+                    
+                    if (matrix_in(Matrix2VectorIndexConvertion(
+                        input_matrix_width, stride * i + p, stride * j + q))
+                        > local_max_var) then
+                        local_max_var := matrix_in(
+                            Matrix2VectorIndexConvertion(
+                            input_matrix_width, stride * i + p, stride * j + q
+                            )
+                            );
+                    end if;
+                end loop;
+            end loop;
+        matrix_return(Matrix2VectorIndexConvertion(
+            output_matrix_width, i, j)) := local_max_var;
+        end loop;
+    end loop;
+    RETURN matrix_return;
+END;
+
+BEGIN
+
+matrix_out <= MaxPoolingFunction(matrix_in);
+
+end MaxPoolBehavioral;
